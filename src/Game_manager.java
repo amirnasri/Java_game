d0ed04e7b93b49c9a517ad880df4baf0b313a05d
Amir <amir@fortinet>
2015-01-28 17:56:29 -0800

    added Game manager code.

--- src/Game_manager.java
+++ src/Game_manager.java
@@ -1,30 +1,64 @@
+import java.awt.Graphics;
 import java.awt.Image;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Game_manager {
+
+public class Game_manager extends JFrame {
 
 	private Image[][] tile_map;
-	private HashMap<Character, Image> game_images; 
+	private HashMap<Character, Image> game_images;
+	private JPanel panel;
+	
+	public Game_manager() {
+		
+		panel = new JPanel() {
+			
+			@Override
+			protected void paintComponent(Graphics g) {
+				g.drawString("Hello", 100, 100);
+			}
+		};
+		add(panel);
+	}
+	
 	
 	private void load_game_images() {
+		ImageIcon im = new ImageIcon("images/player1.png");
 		
 	}
 	
-	public static void main(String[] args) {
-		
-		BufferedReader f = new BufferedReader(new FileReader("tile_map"));
-		String line;
+	private void load_tile_map() throws IOException {
 		int row = 0;
+		String line;
+		BufferedReader f = new BufferedReader(new FileReader("tile_map"));
+		
 		while ((line = f.readLine()) != null) {
 			row += 1;
 			for (int col = 0; col < line.length(); col++) {
-				char tile = line.charAt(i);
-				tile_map[row, col] = game_images.get(tile); 
+				char tile = line.charAt(col);
+				tile_map[row][col] = game_images.get(tile); 
 			}
 		}
+		f.close();
+	}
+
+	
+	public void start() {
+		
 	}
 	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Game_manager gm = new Game_manager();
+		gm.start();
+		
+	}	
 }
